6af98301be844d8cb05576f57715c4cb386f7a51
Lorenzo Mangani <lorenzo.mangani@gmail.com>
2022-10-17 17:26:15 +0200

    embedded version

---
+++ src/App.tsx
@@ -0,0 +1,41 @@
+import { ReactReplView } from 'awesome-react-repl';
+import { useRef, useState } from 'react';
+const REACT_APP_API_URL = process.env.REACT_APP_API_URL || window.location.href;
+
+const query = (input: string) => {
+    const URL = REACT_APP_API_URL+'/?query='+input;
+    const request = new XMLHttpRequest();
+    request.open('GET', URL, false);  // `false` makes the request synchronous
+    request.setRequestHeader("custom-header", "test")
+    request.send(null);
+    if (request.status !== 200) {
+      // handle an error here
+      return
+    }
+    return request.responseText;
+}
+
+function App() {
+  const [lines, setLines] = useState<any[]>([]);
+
+  function handleSubmit(input: string) {
+    setLines(lines => lines.concat({ type: 'input', value: input }));
+    try {
+      const output = query(input);
+      setLines(lines => lines.concat({ type: 'output', value: output }));
+    } catch (e: any) {
+      setLines(lines => lines.concat({ type: 'error', value: e.toString() }));
+    }
+  }
+
+  return (
+    <ReactReplView
+      title="Ratlab"
+      lines={lines}
+      onSubmit={handleSubmit}
+      height={window.innerHeight}
+    />
+  );
+}
+
+export default App;
